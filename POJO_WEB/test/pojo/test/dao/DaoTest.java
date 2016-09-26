3bb8d996d584623db16b766b8190090f0550d41a
tslav <tslav@DESKTOP-5719799>
2016-09-26 18:16:26 +0900

    dd

---
+++ POJO_WEB/test/pojo/test/dao/DaoTest.java
@@ -0,0 +1,14 @@
+package pojo.test.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
