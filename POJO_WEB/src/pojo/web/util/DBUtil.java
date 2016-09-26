46d329863653643bb1ba3c69ef705d8dd312ae68
tslav <tslav@DESKTOP-5719799>
2016-09-26 21:43:08 +0900

    src

--- POJO_WEB/src/pojo/web/util/DBUtil.java
+++ POJO_WEB/src/pojo/web/util/DBUtil.java
@@ -1,5 +1,40 @@
 package pojo.web.util;
 
-public class DBUtil {
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DBUtil {
+	static Logger logger = LoggerFactory.getLogger(DBUtil.class);
+
+	private DBUtil() {
+		init();
+	}
+
+	private static DBUtil instance = new DBUtil();
+
+	public static DBUtil getInstance() {
+		return instance;
+	}
+	
+	private SqlSessionFactory factory;
+	
+	private void init(){
+		String res = "mybatis-config.xml";
+		try{
+			InputStream input = Resources.getResourceAsStream(res);
+			factory = new SqlSessionFactoryBuilder().build(input);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public SqlSession getSession(){
+		return factory.openSession();
+	}
 }
