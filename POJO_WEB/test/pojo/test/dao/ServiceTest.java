73ae153492b630152293cb76f4fa0067f8dc8bfd
1-718-8 <1-718-8@01-717-8-PC>
2016-09-26 18:22:05 +0900

    DDF

---
+++ POJO_WEB/test/pojo/test/dao/ServiceTest.java
@@ -0,0 +1,14 @@
+package pojo.test.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
