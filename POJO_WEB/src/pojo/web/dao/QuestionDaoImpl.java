45732058724b25f414ac278be53d03a342d04aad
tslav <tslav@DESKTOP-5719799>
2016-09-30 14:47:10 +0900

    dto

--- POJO_WEB/src/pojo/web/dao/QuestionDaoImpl.java
+++ POJO_WEB/src/pojo/web/dao/QuestionDaoImpl.java
@@ -1,5 +1,41 @@
 package pojo.web.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import pojo.web.dto.Question;
+
 public class QuestionDaoImpl implements QuestionDao {
+
+	@Override
+	public List<Question> selectAllQuestion(SqlSession session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Question selectByQuestNo(SqlSession session, int questNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertQuestion(SqlSession session, Question question) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateQuestion(SqlSession session, Question question) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteQuestion(SqlSession session, int questNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
