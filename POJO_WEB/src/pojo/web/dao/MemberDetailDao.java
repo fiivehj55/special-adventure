108158b09e58ee253b6118bf9f1cbf0529bd4889
tslav <tslav@DESKTOP-5719799>
2016-09-30 14:31:56 +0900

    dto

--- POJO_WEB/src/pojo/web/dao/MemberDetailDao.java
+++ POJO_WEB/src/pojo/web/dao/MemberDetailDao.java
@@ -1,5 +1,21 @@
 package pojo.web.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import pojo.web.dto.MemberDetail;
+
 public interface MemberDetailDao {
+	
+	List<MemberDetail> selectAllMemberDetail(SqlSession session);
+	
+	MemberDetail selectByMemberDetail(SqlSession session, int detailId);
+	
+	int insertMemberDetail(SqlSession session, MemberDetail mDetail);
+	
+	int updateMemberDetail(SqlSession session, MemberDetail mDetail);
+	
+	int deleteMemberDetail(SqlSession session, int deltailId);
 	
 }
